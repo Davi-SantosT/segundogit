e06685fab66439a433ac28b414719899700f7bd2
Seu Nome <daviprofissonal07@gmail.com>
2025-04-23 20:07:20 -0300

    main.yml

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-'console.log("Ol , mundo!");' 
+const express = require("express");  
+const app = express();  
+
+const PORT = 8884;  
+
+app.use((req, res, next) => {  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();  
+});  
+
+app.get("/", (req, res) => {  
+    res.send("Servidor rodando na porta 8884! ðŸš€");  
+});  
+
+app.listen(PORT, () => {  
+    console.log(`Servidor iniciado em http://localhost:${PORT}`);  
+});
